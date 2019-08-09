b6733f4fa3b0f9c9eac3e83a3d587eb906176f1a
Andrew Leader <aleader@microsoft.com>
2019-08-09 15:34:45 -0700

    Add PostBack to server

---
+++ src/InteractiveNotifs.Hub/Controllers/Api/PostBackController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InteractiveNotifs.Hub.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostBackController : ControllerBase
+    {
+        // POST: api/PostBack
+        [HttpPost]
+        public ActionResult Post()
+        {
+            using (StreamReader reader = new StreamReader(Request.Body))
+            {
+                string reqBody = reader.ReadToEnd();
+                return Ok(reqBody);
+            }
+        }
+    }
+}
